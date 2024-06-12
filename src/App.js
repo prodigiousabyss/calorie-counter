dea185cf4d295e5ad25fe0e16ff003393b542ec0
Pooja <poojaghanghas479@gmail.com>
2024-06-12 14:28:24 +0530

    home-page layout design v0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { Navbar } from './components/Navbar/Navbar'
-import { Topbar } from './components/Topbar/Topbar'
+import { Routes, Route } from 'react-router-dom'
+import { Navbar } from './pages/Navbar/Navbar'
+import { Home } from './pages/Home/Home'
+import { Profile } from './pages/Profile/Profile'
+import { Scanner } from './pages/Scanner/Scanner'
 
 function App() {
     return (
-        <div className="App">
+        <div className="flex flex-col h-screen">
+            <div className="grow overflow-auto">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/scan" element={<Scanner />} />
+                </Routes>
+            </div>
             <Navbar />
-            <Topbar />
         </div>
     )
 }
